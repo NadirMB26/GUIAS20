aa1e49ee693864c95939dae635d5930d5d2646c6
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 18:45:53 -0500

    primer guia2

---
+++ Jradiobutton/src/main/java/com/mycompany/jradiobutton/Jradiobutton.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.jradiobutton;
+
+import igui.pantallaj;
+
+/**
+ *
+ * @author nadir
+ */
+public class Jradiobutton {
+
+    public static void main(String[] args) {
+         new pantallaj().setVisible(true);
+         
+    }
+}
