2924478a250060334c4d899247d1fcb82be6d738
NadirMarsiglia <nadir.marsigliababilonia@unicolombo.edu.co>
2024-04-21 19:12:51 -0500

    primer GUIA17

---
+++ ProyectoCalculos/src/main/java/com/mycompany/proyectocalculos/ProyectoCalculos.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectocalculos;
+
+import igui.pcpantalla;
+
+/**
+ *
+ * @author nadir
+ */
+public class ProyectoCalculos {
+
+    public static void main(String[] args) {
+      new pcpantalla().setVisible(true);
+    }
+}
